ab1ca033c3276d4cce5212dcd03abce5b958b003
Estefany Ochoa Romero <tefaoch23@gmail.com>
2025-09-12 09:29:28 +0200

    [FIX] solution problem with name control in the view website in contact us form

---
+++ crm_lead_custom/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CRM Lead Custom",
+
+    'summary': "Customizations for CRM leads and website contact form",
+
+    'description': """
+Customizes CRM Leads and Website Contact form:
+- Adds 'Source Type' field in CRM.
+- Updates website contact form (remove phone, company optional, source required, email validation).
+""",
+
+    'author': "Binovo",
+    'website': "https://www.yourcompany.com",
+
+    # Category
+    'category': 'Website',
+    'version': '18.0.1.0.1',
+
+    # Modules required for this module to work
+    'depends': ["crm", "website_crm"],
+
+    # Always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        "views/crm_lead_views.xml",
+        "views/website_crm_form.xml",
+    ],
+
+    # Only loaded in demonstration mode (optional)
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
---
+++ crm_lead_custom/models/crm_lead.py
@@ -0,0 +1,21 @@
+from odoo import fields, models,  api
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    source_type = fields.Selection(
+        [
+            ("third_party", "Terceros"),
+            ("social_media", "Redes sociales"),
+            ("internet", "Búsqueda en Internet"),
+        ],
+        string="Source Type",
+        required=False,
+    )
+
+    @api.model
+    def create_from_website(self, vals):
+        if not vals.get('name'):
+            vals['name'] = "Not name"
+        return super(CrmLead, self).create(vals)
